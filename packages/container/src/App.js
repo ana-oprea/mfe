442991482504fa2b51c5ce6a8f5e087052fc547f
ana-oprea <anaop3212@gmail.com>
2022-10-05 16:35:14 +0300

    add region

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from "./components/MarketingApp"
 export default () => {
   return (
     <div>
-      <h1>hhi there!</h1>
+      <h1>hhhhi there!</h1>
       <hr />
       <MarketingApp />
     </div>
