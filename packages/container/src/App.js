b5bc93b9808b5dac1aeef4aaaa7fe416c54bb5a8
ana-oprea <anaop3212@gmail.com>
2022-10-05 16:02:59 +0300

    testing action

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from "./components/MarketingApp"
 export default () => {
   return (
     <div>
-      <h1>hi there!</h1>
+      <h1>hhi there!</h1>
       <hr />
       <MarketingApp />
     </div>
